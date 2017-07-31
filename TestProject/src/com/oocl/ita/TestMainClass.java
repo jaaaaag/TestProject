85b9d4ab546b46d2b4731bdb47070c36d63fb488
GERVAJO <GERVAJO@ZHA-ITA129-W7.corp.oocl.com>
2017-07-31 15:16:28 +0800

    Test 2

--- TestProject/src/com/oocl/ita/TestMainClass.java
+++ TestProject/src/com/oocl/ita/TestMainClass.java
@@ -5,6 +5,7 @@ public class TestMainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test Class");
+		System.out.println("Test 2");
 
 	}
 
