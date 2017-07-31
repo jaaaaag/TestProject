5bb2d9238b8ea75fceab837c9ab88bd0e2c33fee
GERVAJO <GERVAJO@ZHA-ITA129-W7.corp.oocl.com>
2017-07-31 15:10:39 +0800

    Test Push

---
+++ TestProject/src/com/oocl/ita/TestMainClass.java
@@ -0,0 +1,11 @@
+package com.oocl.ita;
+
+public class TestMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Class");
+
+	}
+
+}
